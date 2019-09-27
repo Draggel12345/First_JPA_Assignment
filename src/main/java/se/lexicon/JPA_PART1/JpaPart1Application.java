6bdd970e0092b6db5df365d98b8d15b63e93c9b3
Draggel12345 <anton.edholm@hotmail.com>
2019-09-27 09:44:23 +0200

    First_JPA_Assignment

---
+++ src/main/java/se/lexicon/JPA_PART1/JpaPart1Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.JPA_PART1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPart1Application.class, args);
+	}
+
+}
